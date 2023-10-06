2d5f71d8f8b3ee9ae2c71d26998a1320f3a42ec2
Jef Driesen <jef@libdivecomputer.org>
2023-10-06 16:16:17 +0200

    Add the sample api implementation

--- org_libdivecomputer_Parser.c
+++ org_libdivecomputer_Parser.c
@@ -6,7 +6,20 @@ typedef struct jni_parser_t {
 	JNIEnv *env;
 	jobject obj;
 	jclass cls;
-	jmethodID method;
+	jmethodID time;
+	jmethodID depth;
+	jmethodID pressure;
+	jmethodID temperature;
+	jmethodID event;
+	jmethodID rbt;
+	jmethodID heartbeat;
+	jmethodID bearing;
+	jmethodID vendor;
+	jmethodID setpoint;
+	jmethodID ppo2;
+	jmethodID cns;
+	jmethodID deco;
+	jmethodID gasmix;
 } jni_parser_t;
 
 static void
@@ -14,8 +27,92 @@ sample_cb (dc_sample_type_t type, const dc_sample_value_t *value, void *userdata
 {
 	jni_parser_t *jni = userdata;
 
-	(*jni->env)->CallVoidMethod(jni->env, jni->obj, jni->method,
-		type);
+	switch (type) {
+	case DC_SAMPLE_TIME:
+		(*jni->env)->CallVoidMethod(jni->env, jni->obj,
+			jni->time,
+			value->time);
+		break;
+	case DC_SAMPLE_DEPTH:
+		(*jni->env)->CallVoidMethod(jni->env, jni->obj,
+			jni->depth,
+			value->depth);
+		break;
+	case DC_SAMPLE_PRESSURE:
+			(*jni->env)->CallVoidMethod(jni->env, jni->obj,
+			jni->pressure,
+			value->pressure.tank,
+			value->pressure.value);
+		break;
+	case DC_SAMPLE_TEMPERATURE:
+		(*jni->env)->CallVoidMethod(jni->env, jni->obj,
+			jni->temperature,
+			value->temperature);
+		break;
+	case DC_SAMPLE_EVENT:
+		(*jni->env)->CallVoidMethod(jni->env, jni->obj,
+			jni->event,
+			value->event.type,
+			value->event.time,
+			value->event.flags,
+			value->event.value);
+		break;
+	case DC_SAMPLE_RBT:
+		(*jni->env)->CallVoidMethod(jni->env, jni->obj,
+			jni->rbt,
+			value->rbt);
+		break;
+	case DC_SAMPLE_HEARTBEAT:
+		(*jni->env)->CallVoidMethod(jni->env, jni->obj,
+			jni->heartbeat,
+			value->heartbeat);
+		break;
+	case DC_SAMPLE_BEARING:
+		(*jni->env)->CallVoidMethod(jni->env, jni->obj,
+			jni->bearing,
+			value->bearing);
+		break;
+	case DC_SAMPLE_VENDOR:
+#if 0
+		(*jni->env)->CallVoidMethod(jni->env, jni->obj,
+			jni->vendor,
+			value->vendor.type,
+			value->vendor.data,
+			value->vendor.size);
+#endif
+		break;
+	case DC_SAMPLE_SETPOINT:
+		(*jni->env)->CallVoidMethod(jni->env, jni->obj,
+			jni->setpoint,
+			value->setpoint);
+		break;
+	case DC_SAMPLE_PPO2:
+		(*jni->env)->CallVoidMethod(jni->env, jni->obj,
+			jni->ppo2,
+			value->ppo2.sensor,
+			value->ppo2.value);
+		break;
+	case DC_SAMPLE_CNS:
+		(*jni->env)->CallVoidMethod(jni->env, jni->obj,
+			jni->cns,
+			value->cns);
+		break;
+	case DC_SAMPLE_DECO:
+		(*jni->env)->CallVoidMethod(jni->env, jni->obj,
+			jni->deco,
+			value->deco.type,
+			value->deco.time,
+			value->deco.depth,
+			value->deco.tts);
+		break;
+	case DC_SAMPLE_GASMIX:
+		(*jni->env)->CallVoidMethod(jni->env, jni->obj,
+			jni->gasmix,
+			value->gasmix);
+		break;
+	default:
+		break;
+	}
 }
 
 JNIEXPORT jlong JNICALL Java_org_libdivecomputer_Parser_New
@@ -78,7 +175,20 @@ JNIEXPORT void JNICALL Java_org_libdivecomputer_Parser_Foreach
 	jni.env = env;
 	jni.obj = (*env)->NewGlobalRef(env, callback);
 	jni.cls = (*env)->GetObjectClass(env, callback);
-	jni.method = (*env)->GetMethodID(env, jni.cls, "Sample", "(I)V");
+	jni.time        = (*env)->GetMethodID(env, jni.cls, "Time", "(I)V");
+	jni.depth       = (*env)->GetMethodID(env, jni.cls, "Depth", "(D)V");
+	jni.pressure    = (*env)->GetMethodID(env, jni.cls, "Pressure", "(ID)V");
+	jni.temperature = (*env)->GetMethodID(env, jni.cls, "Temperature", "(D)V");
+	jni.event       = (*env)->GetMethodID(env, jni.cls, "Event", "(IIII)V");
+	jni.rbt         = (*env)->GetMethodID(env, jni.cls, "Rbt", "(I)V");
+	jni.heartbeat   = (*env)->GetMethodID(env, jni.cls, "Heartbeat", "(I)V");
+	jni.bearing     = (*env)->GetMethodID(env, jni.cls, "Bearing", "(I)V");
+	jni.vendor      = (*env)->GetMethodID(env, jni.cls, "Vendor", "(I[B)V");
+	jni.setpoint    = (*env)->GetMethodID(env, jni.cls, "Setpoint", "(D)V");
+	jni.ppo2        = (*env)->GetMethodID(env, jni.cls, "Ppo2", "(ID)V");
+	jni.cns         = (*env)->GetMethodID(env, jni.cls, "Cns", "(D)V");
+	jni.deco        = (*env)->GetMethodID(env, jni.cls, "Deco", "(IIDI)V");
+	jni.gasmix      = (*env)->GetMethodID(env, jni.cls, "Gasmix", "(I)V");
 
 	dc_parser_samples_foreach((dc_parser_t *) handle, sample_cb, &jni);
 
